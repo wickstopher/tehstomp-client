1e5c1280bf8cf76ac9c124d7f63422a617bd1910
Christopher R. Wicks <wickstopher@gmail.com>
2017-09-17 11:25:15 -0400

    Generic frame construction.

--- Stomp/Frames.hs
+++ Stomp/Frames.hs
@@ -1,11 +1,11 @@
 data Header         =   Header HeaderName HeaderValue
-type Headers        =   [Header]
+data Headers        =   Some Header Headers | EndOfHeaders
 type HeaderName     =   String
 type HeaderValue    =   String
 
-type Body           =   String
+data Body           =   EmptyBody | Body String
 
-data ClientCommand  =   SEND |
+data Command        =   SEND |
                         SUBSCRIBE |
                         UNSUBSCRIBE |
                         BEGIN |
@@ -15,20 +15,35 @@ data ClientCommand  =   SEND |
                         NACK |
                         DISCONNECT |
                         CONNECT |
-                        STOMP deriving Show
-
-data ServerCommand  =   CONNECTED |
+                        STOMP |
+                        CONNECTED |
                         MESSAGE |
                         RECEIPT |
                         ERROR deriving Show
 
-data Command        =   S ServerCommand | C ClientCommand
+data Frame          =   Frame Command Headers Body
 
-type Frame          =   (Command, Maybe Headers, Maybe Body)
+instance Show Header where
+    show (Header headerName headerValue) = headerName ++ ":" ++ headerValue
+
+instance Show Headers where
+    show EndOfHeaders = ""
+    show (Some header headers) = show header ++ "\n" ++ show headers
+
+instance Show Body where
+    show EmptyBody = ""
+    show (Body s)    = s
+
+instance Show Frame where
+    show (Frame c h b) = show c ++ "\n" ++ show h ++ show b ++ "\NUL"
+
+
+listToHeaders :: [Header] -> Headers
+listToHeaders []     = EndOfHeaders
+listToHeaders (x:xs) = Some x (listToHeaders xs)
 
 connectHeaders :: Headers
-connectHeaders = [Header "accept-version" "1.2", Header "host" "wickstopher.com"]
+connectHeaders = listToHeaders [Header "accept-version" "1.2", Header "host" "wickstopher.com"]
 
 makeConnect :: Frame
-makeConnect = (C CONNECT, Just connectHeaders, Nothing)
-
+makeConnect = Frame CONNECT connectHeaders EmptyBody
