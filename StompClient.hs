10d5474b69e8c5eb1e3d313ab124ef1de7810608
Christopher R. Wicks <wickstopher@gmail.com>
2017-09-30 19:30:20 -0400

    More test client code.

--- StompClient.hs
+++ StompClient.hs
@@ -7,4 +7,19 @@ main :: IO ()
 main = do
     handle <- Network.connectTo "localhost" (PortNumber 2323)
     hPut handle (frameToBytes (connect "hi"))
+    hPut handle (frameToBytes (sendText "hi" "someq"))
+    hPut handle (frameToBytes (sendText "bye" "someq"))
+    hPut handle (frameToBytes (disconnect "buhuug"))
     hClose handle
+
+sendFrames :: [Frame] -> IO ()
+sendFrames frames = do
+    handle <- Network.connectTo "localhost" (PortNumber 2323)
+    processFrames handle frames
+    hClose handle
+
+processFrames :: Handle -> [Frame] -> IO ()
+processFrames handle [] = return ()
+processFrames handle (frame:frames) = do
+    hPut handle (frameToBytes frame)
+    processFrames handle frames
