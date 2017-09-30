b3c4c720408c397c55180a5c741794801e66139d
Christopher R. Wicks <wickstopher@gmail.com>
2017-09-30 15:17:01 -0400

    Using ByteStrings instead.

--- StompClient.hs
+++ StompClient.hs
@@ -1,9 +1,10 @@
 import Network as Network
 import System.IO
+import Data.ByteString
 import Stomp.Frames
 
 main :: IO ()
 main = do
     handle <- Network.connectTo "localhost" (PortNumber 2323)
-    hPutStr handle (show (connect "hi"))
+    hPut handle (frameToBytes (connect "hi"))
     hClose handle
