7d383bf58a0b3268a7b36d4ee52458510ea0e87b
Christopher R. Wicks <wickstopher@gmail.com>
2017-10-02 21:45:28 -0400

    Command to display session information.

--- StompClient.hs
+++ StompClient.hs
@@ -7,48 +7,61 @@ import Stomp.Frames
 import Stomp.Frames.IO
 import Stomp.Util
 
-main :: IO (Maybe Handle)
+data Session = Session Handle String String | Disconnected
+
+instance Show Session where
+    show (Session h ip port) = "Connected to broker at " ++ ip ++ ":" ++ port
+    show Disconnected        = "Session is not connected"
+
+main :: IO Session
 main = do
     hSetBuffering stdout NoBuffering
-    prompt Nothing
+    prompt Disconnected
 
-prompt :: Maybe Handle -> IO (Maybe Handle)
-prompt handle = do
+prompt :: Session -> IO Session
+prompt session = do
     putStr "<STOMP> "
     input <- getLine
-    newHandle <- try (processInput (tokenize " " input) handle) :: IO (Either SomeException (Maybe Handle))
-    case newHandle of 
+    result <- try (processInput (tokenize " " input) session) :: IO (Either SomeException Session)
+    case result of 
         Left exception -> do
             putStrLn  ("Caught exception: " ++ (show exception))
-            prompt handle
-        Right newHandle -> prompt newHandle
+            prompt session
+        Right session' -> prompt session'
 
-processInput :: [String] -> Maybe Handle -> IO (Maybe Handle)
-processInput [] handle = do return handle
+processInput :: [String] -> Session -> IO Session
+processInput [] session = do return session
 
-processInput ("connect":ip:p:[]) handle = do
+processInput ("session":[]) session = do
+    putStrLn $ show session
+    return session
+
+processInput ("connect":ip:p:[]) Disconnected = do
     newHandle <- Network.connectTo ip (portFromString p)
     hPut newHandle (frameToBytes $ connect "nohost")
     response <- parseFrame newHandle
     case response of
         (Frame CONNECTED _ _) -> do
             putStrLn $ "Connected to " ++ ip ++ " on port " ++ p
-            return $ Just newHandle
+            return $ Session newHandle ip p
         (Frame ERROR _ body) -> do
             putStrLn "There was a problem connecting: "
             putStrLn (show body)
-            return handle
+            return Disconnected
 
-processInput ("send":queue:message) h@(Just handle) = do
-    hPut handle (frameToBytes $ sendText (intercalate " " message) queue)
-    return h
-processInput ("send":_) Nothing = do
+processInput ("send":_) Disconnected = do
     putStrLn "You must initiate a connection before sending a message"
-    return Nothing
+    return Disconnected
+processInput ("send":queue:message) session = do
+    sendFrame session $ sendText (intercalate " " message) queue
+    return session
 
-processInput ("sendr":queue:receiptId:message) h@(Just handle) = do
-    hPut handle (frameToBytes $ addReceiptHeader receiptId $ (sendText (intercalate " " message) queue))
-    response <- parseFrame handle
+processInput ("sendr":_) Disconnected = do
+    putStrLn "You must initiate a connection before sending a message"
+    return Disconnected
+processInput ("sendr":queue:receiptId:message) session = do
+    sendFrame session $ addReceiptHeader receiptId (sendText (intercalate " " message) queue)
+    response <- receiveFrame session
     case response of
         (Frame RECEIPT _ _) -> do
             case (getReceiptId response) of
@@ -57,14 +70,17 @@ processInput ("sendr":queue:receiptId:message) h@(Just handle) = do
         (Frame ERROR _ body) -> do
             putStrLn "There was a problem: "
             putStrLn (show body)
-    return h
-processInput ("sendr":_) Nothing = do
-    putStrLn "You must initiate a connection before sending a message"
-    return Nothing
+    return session
 
-processInput _ handle = do
+processInput _ session = do
     putStrLn "Unrecognized or malformed command"
-    return handle
+    return session
 
 portFromString :: String -> PortID
 portFromString s = PortNumber (fromIntegral ((read s)::Int))
+
+sendFrame :: Session -> Frame -> IO ()
+sendFrame (Session handle _ _) frame = do hPut handle $ frameToBytes frame
+
+receiveFrame :: Session -> IO Frame
+receiveFrame (Session handle _ _) = parseFrame handle
