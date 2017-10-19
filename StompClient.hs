f6da5e057b993a1e68e5ca775ea15f664ac05dc5
Christopher R. Wicks <wickstopher@gmail.com>
2017-10-18 21:00:15 -0400

    Add function comments.

--- StompClient.hs
+++ StompClient.hs
@@ -12,29 +12,33 @@ import Stomp.Frames.IO
 import qualified Stomp.TLogger as TLog
 import Stomp.Util
 
-data Session = Session FrameHandler String String TLog.Logger RequestHandler (SChan FrameEvt) | Disconnected TLog.Logger
+-- A Session is either Disconnected or represents an open connection with a STOMP broker.
+data Session = Disconnected TLog.Logger | Session FrameHandler String String TLog.Logger RequestHandler (SChan FrameEvt) 
 
 instance Show Session where
     show (Session h ip port _  _ _) = "Connected to broker at " ++ ip ++ ":" ++ port
     show (Disconnected _)           = "Session is not connected"
 
+-- Initialize the client
 main :: IO Session
 main = do
     hSetBuffering stdout NoBuffering
     console  <- TLog.initLogger stdout
-    prompt $ Disconnected console
+    promptLoop $ Disconnected console
 
-prompt :: Session -> IO Session
-prompt session = do
+-- Loops, prompting the user for input from stdin.
+promptLoop :: Session -> IO Session
+promptLoop session = do
     sPrompt session "stomp> "
     input <- getLine
     result <- try (processInput (tokenize " " input) session) :: IO (Either SomeException Session)
     case result of 
         Left exception -> do
             sLog session ("[ERROR] " ++ (show exception))
-            prompt session
-        Right session' -> prompt session'
+            promptLoop session
+        Right session' -> promptLoop session'
 
+-- |Process input given on the command-line
 processInput :: [String] -> Session -> IO Session
 
 -- process blank input (return press)
@@ -68,7 +72,6 @@ processInput ("connect":ip:p:[]) session@(Disconnected _) = do
         GotEof -> do
             sLog session "The server disconnected before connections could be negotiated"
             return session
-
 processInput ("connect":_:_:[]) session = do
     sLog session "Please disconnect from your current session before initiating a new one"
     return session
@@ -173,41 +176,52 @@ subscriptionListener console subChan dest subId = do
             TLog.prompt console "\nstomp> "
             return ()
 
+-- |Send a Frame to the Session n times
 loopSend :: Frame -> Session -> Int -> IO Session
 loopSend _ session 0 =  do return session
 loopSend frame session n = do
     sendFrame session frame
     loopSend frame session (n-1)
 
+-- |Convert a String to a PortID
 portFromString :: String -> PortID
 portFromString s = PortNumber (fromIntegral ((read s)::Int))
 
+-- |Send a Frame to the Session
 sendFrame :: Session -> Frame -> IO ()
 sendFrame session frame = put (getHandler session) frame
 
+-- |Disconnect the Session
 disconnectSession :: Session -> IO Session
 disconnectSession session = do
     close $ getHandler session
     return (Disconnected (getLogger session))
 
+-- |Get the FrameHandler from the Session
 getHandler :: Session -> FrameHandler
 getHandler (Session handler _ _ _ _ _) = handler
 
+-- |Get the (console) Logger for the Session
 getLogger :: Session -> TLog.Logger
 getLogger (Session handle _ _ logger _ _) = logger
 getLogger (Disconnected logger) = logger
 
+-- |Get the RequestHandler for the Session
 getRequestHandler :: Session -> RequestHandler
 getRequestHandler (Session _ _ _ _ requestHandler _) = requestHandler
 
+-- |Get the FrameEvt channel for the Session
 getResponseChan :: Session -> (SChan FrameEvt)
 getResponseChan (Session _ _ _ _ _ chan) = chan
 
+-- |Get a response Frame from the Session
 getResponseFrame :: Session -> IO FrameEvt
 getResponseFrame session = sync $ recvEvt (getResponseChan session)
 
+-- |Log a message to the session
 sLog :: Session -> String -> IO ()
 sLog session message = TLog.log (getLogger session) message
 
+-- |Log a message to the session, but do not append a newline character (e.g. for a prompt)
 sPrompt :: Session -> String -> IO ()
 sPrompt session message = TLog.prompt (getLogger session) message
